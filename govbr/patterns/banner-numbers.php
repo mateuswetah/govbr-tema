f79ce74403d5d72041970d324c53a22d6aa47d60
mateuswetah <mateus.m.luna@gmail.com>
2023-04-24 14:09:23 -0300

    Adiciona padrão de blocos de banner com números #7.

---
+++ govbr/patterns/banner-numbers.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Title: Banner with numbers
+ * Slug: govbr/banner-numbers
+ * Categories: banners, govbr
+ */
+?>
+<!-- wp:cover {"overlayColor":"surface-dark-alternative","minHeight":383,"minHeightUnit":"px","contentPosition":"center center","align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull" style="min-height:383px"><span aria-hidden="true" class="wp-block-cover__background has-surface-dark-alternative-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":"1.2"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"is-style-ctrls-titulo","fontSize":"large"} -->
+<h2 class="wp-block-heading has-text-align-center is-style-ctrls-titulo has-large-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;line-height:1.2">Desempenho dos serviços</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|scale-up-6","padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"top"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"900","lineHeight":"1.2"}},"fontSize":"scale-up-07"} -->
+<p class="has-text-align-center has-scale-up-07-font-size" style="font-style:normal;font-weight:900;line-height:1.2">4769</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"scale-up-02"} -->
+<p class="has-text-align-center has-scale-up-02-font-size" style="font-style:normal;font-weight:500">Serviços no portal</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"900","lineHeight":"1.2"}},"fontSize":"scale-up-07"} -->
+<p class="has-text-align-center has-scale-up-07-font-size" style="font-style:normal;font-weight:900;line-height:1.2">89%</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"scale-up-02"} -->
+<p class="has-text-align-center has-scale-up-02-font-size" style="font-style:normal;font-weight:500">Serviços Digitais</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"900","lineHeight":"1.2"}},"fontSize":"scale-up-07"} -->
+<p class="has-text-align-center has-scale-up-07-font-size" style="font-style:normal;font-weight:900;line-height:1.2">53%</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"scale-up-02"} -->
+<p class="has-text-align-center has-scale-up-02-font-size" style="font-style:normal;font-weight:500">Avaliações Positivas</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Ver mais indicadores</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover -->

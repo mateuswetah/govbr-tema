3647296664bc6832364296fcbfcee66821bf07ed
mateuswetah <mateus.m.luna@gmail.com>
2023-05-23 10:21:18 -0300

    Inicia collapses do footer escondido para telas mobile. #5 e #6.

--- govbr/patterns/footer-links.php
+++ govbr/patterns/footer-links.php
@@ -23,11 +23,11 @@ $image_placeholder_2 = esc_url( get_template_directory_uri() ) . '/assets/images
 <div class="wp-block-columns br-list horizontal" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"width":"25%"} -->
 <div class="wp-block-column" style="flex-basis:25%"><!-- wp:govbr/collapse {"blockId":"f4514252-a046-4a11-b15e-ad948a053776"} -->
 <div class="wp-block-govbr-collapse br-list"><!-- wp:govbr/collapse-label {"parentBlockId":"f4514252-a046-4a11-b15e-ad948a053776"} -->
-<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse" data-target="brgov-collapse--f4514252-a046-4a11-b15e-ad948a053776"><div class="content"><div class="flex-fill"><?php _e('A list of links', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
+<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse"  data-breakpoint="false" data-target="brgov-collapse--f4514252-a046-4a11-b15e-ad948a053776"><div class="content"><div class="flex-fill"><?php _e('A list of links', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
 <!-- /wp:govbr/collapse-label -->
 
 <!-- wp:govbr/collapse-content {"parentBlockId":"f4514252-a046-4a11-b15e-ad948a053776"} -->
-<div id="brgov-collapse--f4514252-a046-4a11-b15e-ad948a053776" class="wp-block-govbr-collapse-content"><div><!-- wp:list {"className":"is-style-govbr-list"} -->
+<div id="brgov-collapse--f4514252-a046-4a11-b15e-ad948a053776" class="wp-block-govbr-collapse-content" hidden><div><!-- wp:list {"className":"is-style-govbr-list"} -->
 <ul class="is-style-govbr-list"><!-- wp:list-item -->
 <li><a href="https://www.gov.br/ds"><?php _e('Item of the list', 'govbr') ?> 1</a></li>
 <!-- /wp:list-item -->
@@ -51,11 +51,11 @@ $image_placeholder_2 = esc_url( get_template_directory_uri() ) . '/assets/images
 <!-- wp:column {"width":"25%"} -->
 <div class="wp-block-column" style="flex-basis:25%"><!-- wp:govbr/collapse {"blockId":"d40ef1a4-bc02-4d5c-ad3d-5dffe3831350"} -->
 <div class="wp-block-govbr-collapse br-list"><!-- wp:govbr/collapse-label {"parentBlockId":"d40ef1a4-bc02-4d5c-ad3d-5dffe3831350"} -->
-<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse" data-target="brgov-collapse--d40ef1a4-bc02-4d5c-ad3d-5dffe3831350"><div class="content"><div class="flex-fill"><?php _e('Website pages', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
+<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse"  data-breakpoint="true" data-target="brgov-collapse--d40ef1a4-bc02-4d5c-ad3d-5dffe3831350"><div class="content"><div class="flex-fill"><?php _e('Website pages', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
 <!-- /wp:govbr/collapse-label -->
 
 <!-- wp:govbr/collapse-content {"parentBlockId":"d40ef1a4-bc02-4d5c-ad3d-5dffe3831350"} -->
-<div id="brgov-collapse--d40ef1a4-bc02-4d5c-ad3d-5dffe3831350" class="wp-block-govbr-collapse-content"><div><!-- wp:page-list {"className":"is-style-govbr-list"} /--></div></div>
+<div id="brgov-collapse--d40ef1a4-bc02-4d5c-ad3d-5dffe3831350" class="wp-block-govbr-collapse-content" hidden><div><!-- wp:page-list {"className":"is-style-govbr-list"} /--></div></div>
 <!-- /wp:govbr/collapse-content --></div>
 <!-- /wp:govbr/collapse --></div>
 <!-- /wp:column -->
@@ -63,11 +63,11 @@ $image_placeholder_2 = esc_url( get_template_directory_uri() ) . '/assets/images
 <!-- wp:column {"width":"25%"} -->
 <div class="wp-block-column" style="flex-basis:25%"><!-- wp:govbr/collapse {"blockId":"9d03aef0-9cb5-40cb-b8c2-de64a658e2d0"} -->
 <div class="wp-block-govbr-collapse br-list"><!-- wp:govbr/collapse-label {"parentBlockId":"9d03aef0-9cb5-40cb-b8c2-de64a658e2d0"} -->
-<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse" data-target="brgov-collapse--9d03aef0-9cb5-40cb-b8c2-de64a658e2d0"><div class="content"><div class="flex-fill"><?php _e('Website categories', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
+<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse"  data-breakpoint="true" data-target="brgov-collapse--9d03aef0-9cb5-40cb-b8c2-de64a658e2d0"><div class="content"><div class="flex-fill"><?php _e('Website categories', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
 <!-- /wp:govbr/collapse-label -->
 
 <!-- wp:govbr/collapse-content {"parentBlockId":"9d03aef0-9cb5-40cb-b8c2-de64a658e2d0"} -->
-<div id="brgov-collapse--9d03aef0-9cb5-40cb-b8c2-de64a658e2d0" class="wp-block-govbr-collapse-content"><div><!-- wp:categories {"className":"is-style-govbr-list"} /--></div></div>
+<div id="brgov-collapse--9d03aef0-9cb5-40cb-b8c2-de64a658e2d0" class="wp-block-govbr-collapse-content" hidden><div><!-- wp:categories {"className":"is-style-govbr-list"} /--></div></div>
 <!-- /wp:govbr/collapse-content --></div>
 <!-- /wp:govbr/collapse --></div>
 <!-- /wp:column -->
@@ -75,11 +75,11 @@ $image_placeholder_2 = esc_url( get_template_directory_uri() ) . '/assets/images
 <!-- wp:column {"width":"25%"} -->
 <div class="wp-block-column" style="flex-basis:25%"><!-- wp:govbr/collapse {"blockId":"262fdfc9-e2aa-4bce-87e6-cd73515c0737"} -->
 <div class="wp-block-govbr-collapse br-list"><!-- wp:govbr/collapse-label {"parentBlockId":"262fdfc9-e2aa-4bce-87e6-cd73515c0737"} -->
-<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse" data-target="brgov-collapse--262fdfc9-e2aa-4bce-87e6-cd73515c0737"><div class="content"><div class="flex-fill"><?php _e('Latest posts', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
+<div class="wp-block-govbr-collapse-label br-item header" data-toggle="collapse"  data-breakpoint="true" data-target="brgov-collapse--262fdfc9-e2aa-4bce-87e6-cd73515c0737"><div class="content"><div class="flex-fill"><?php _e('Latest posts', 'govbr') ?></div><div class="support"><i class="fas fa-angle-down" aria-hidden="true" aria-label="Expand/Collapse"></i></div></div></div>
 <!-- /wp:govbr/collapse-label -->
 
 <!-- wp:govbr/collapse-content {"parentBlockId":"262fdfc9-e2aa-4bce-87e6-cd73515c0737"} -->
-<div id="brgov-collapse--262fdfc9-e2aa-4bce-87e6-cd73515c0737" class="wp-block-govbr-collapse-content"><div><!-- wp:latest-posts {"className":"is-style-govbr-list"} /--></div></div>
+<div id="brgov-collapse--262fdfc9-e2aa-4bce-87e6-cd73515c0737" class="wp-block-govbr-collapse-content" hidden><div><!-- wp:latest-posts {"className":"is-style-govbr-list"} /--></div></div>
 <!-- /wp:govbr/collapse-content --></div>
 <!-- /wp:govbr/collapse --></div>
 <!-- /wp:column --></div>

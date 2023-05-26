d0e620eeafdb6fdf1d1f37726eb0063a176c7f94
mateuswetah <mateus.m.luna@gmail.com>
2023-05-26 16:46:06 -0300

    Estilo básico para archive. #15.

--- govbr/template-parts/header/excerpt-header.php
+++ govbr/template-parts/header/excerpt-header.php
@@ -9,7 +9,7 @@
 
 <header class="entry-header container-lg">
 	<?php
-	the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' );
 	gov_br_post_thumbnail();
+	the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' );
 	?>
 </header><!-- .entry-header -->

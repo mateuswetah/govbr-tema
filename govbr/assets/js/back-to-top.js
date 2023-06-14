85cbed5f3181f7e81badba18db27052b54875930
mateuswetah <mateus.m.luna@gmail.com>
2023-06-14 09:41:12 -0300

    Adiciona debounce para a checagem de scroll do script back-to-top. #22.

--- govbr/assets/js/back-to-top.js
+++ govbr/assets/js/back-to-top.js
@@ -4,24 +4,33 @@
  * Logic for displaying and interacting with the back to top button
  */
 
-const showBackToTop = () => {
-	var backTop = document.querySelector('.back-to-top');
+const backTop = document.querySelector('.back-to-top');
 
+const debounceFunction = (callback, wait) => {
+	let timeoutId = null;
+	return (...args) => {
+		window.clearTimeout(timeoutId);
+		timeoutId = window.setTimeout(() => {
+			callback.apply(null, args);
+		}, wait);
+	};
+}
+
+const showBackToTop = debounceFunction(() => {
 	if ( !backTop ) return;
-
 	window.scrollY > 500
 		? backTop.classList.add('active')
 		: backTop.classList.remove('active');
-}
+}, 500);
 
-showBackToTop();
-
-window.addEventListener('scroll', () => {
-	showBackToTop();
-});
-
-var backTop = document.querySelector('.back-to-top');
 if ( backTop ) {
+
+	showBackToTop();
+
+	window.addEventListener('scroll', () => {
+		showBackToTop();
+	});
+
 	backTop.addEventListener('click', (event) => {
 		event.preventDefault()
 		scrollTo({ top: 0, behavior: 'smooth' });
